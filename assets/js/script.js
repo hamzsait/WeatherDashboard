d0ad6e619cc04d533b83be18927a1b1a69aa6670
hamzsait <hamzasait@utexas.edu>
2021-04-03 14:33:18 -0500

    pushed updated API call

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,25 @@
-function getWeatherAPI(city){
+async function getWeatherAPI(city){
 
-    fetch("https://api.openweathermap.org/data/2.5/weather?q="+city+"&appid=fb58f472da0b2a8c35d45500aeb1b7c1")
+    cityLonLat = await fetch("https://api.openweathermap.org/data/2.5/weather?q="+city+"&appid=fb58f472da0b2a8c35d45500aeb1b7c1")
         .then(function(response){
             return response.json()
         })
         .then (function(data){
-
-            fetch("https://api.openweathermap.org/data/2.5/onecall?lat="+data.coord.lat+"&lon="+data.coord.lon+"&appid=fb58f472da0b2a8c35d45500aeb1b7c1")
-                .then(function(response){
-                    return response.json()
-                })
-                .then (function(data){
-                    console.log(data)
-                })
-
+            return data
         })
 
-    
-
+    outer = await fetch("https://api.openweathermap.org/data/2.5/onecall?lat="+cityLonLat.coord.lat+"&lon="+cityLonLat.coord.lon+"&appid=fb58f472da0b2a8c35d45500aeb1b7c1")
+        .then(function(response){
+            return response.json()
+        })
+        .then (function(data){
+            return data
+        })
+    console.log(outer)
+    return outer
 }
 
-getWeatherAPI("Austin")
+console.log(getWeatherAPI("Austin"))
 
 var submit = document.querySelector("#submit")
 
