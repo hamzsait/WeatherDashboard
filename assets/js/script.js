d633010210a69c43dad6a8d93711c5ad2b9f9974
hamzsait <hamzasait@utexas.edu>
2021-04-03 13:37:12 -0500

    first attempt at Weather API

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,19 @@
-import csv from './jquery.csv.js';
+ 
 
-data = ($(document).ready(function() {
-    $.ajax({
-        type: "GET",
-        url: "worldcities.csv",
-        dataType: "csv",
-     });
-})).toObjects(csv);
+function getWeatherAPI(){
 
-console.log(data)
+    fetch("api.openweathermap.org/data/2.5/weather?q=austin&appid=fb58f472da0b2a8c35d45500aeb1b7c1")
+        .then(response => response.json())
+        .then(data => {console.log(data)})
+}
+
+getWeatherAPI()
 
 var submit = document.querySelector("#submit")
 
 submit.textContent = "SEARCH"
 
-submit.addEventListener(function(event){
+submit.addEventListener(event, function(){
     event.preventDefault()
 
     console.log("hello")
