5520b77b42cdc2b03813ccb8e63498eef1faf30e
hamzsait <hamzasait@utexas.edu>
2021-04-04 18:08:30 -0500

    allowed non-cities to not appear

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,9 +5,11 @@ var temp = document.querySelector("#temp")
 var wind = document.querySelector("#wind")
 var humidity = document.querySelector("#humidity")
 var uvi = document.querySelector("#uvi")
+var pastResults = document.querySelector("#pastResults")
+var warning = document.querySelector("#searchError")
 
 async function getWeatherAPI(city){
-
+    try{
     cityLonLat = await fetch("https://api.openweathermap.org/data/2.5/weather?q="+city+"&appid=fb58f472da0b2a8c35d45500aeb1b7c1")
         .then(function(response){
             return response.json()
@@ -24,18 +26,56 @@ async function getWeatherAPI(city){
             return data
         })
     return outer
+    }
+    catch{
+        return null
+    }
 }
 
 submit.addEventListener("click",function(event){
     event.preventDefault()
 
-    getWeatherAPI(getInput.value).then(function(data){
-        title.textContent = getInput.value.toUpperCase()
+    if (getInput){
+
+        warning.textContent = ""
+
+        getWeatherAPI(getInput.value).then(function(data){
+
+            if (data){
+
+                title.textContent = getInput.value.toUpperCase()
+                temp.textContent = (String(data.current.temp*(9/5)-459.67).slice(0,5) +"°F")
+                wind.textContent = (data.current.wind_speed + " MPH")
+                humidity.textContent = (data.current.humidity + "%")
+                uvi.textContent = (data.current.uvi + " UVI")
+
+                for (x = 0; x < pastResults.children.length; x++){
+                    if (pastResults.children[x].innerText == getInput.value.toUpperCase()){
+                        return
+                    }
+                }
+        
+                item = document.createElement("button")
+                item.textContent = (getInput.value).toUpperCase()
+                item.setAttribute("id","preButton")
+                pastResults.prepend(item)
+            }
+        })
+    }
+
+    else{
+        warning.textContent = "Error, Try Again!"
+    }
+})
+
+$("#pastResults").on("click", "button", function(){
+    var city = this.innerText
+    getWeatherAPI(city).then(function(data){
+        title.textContent = city.toUpperCase()
         temp.textContent = (String(data.current.temp*(9/5)-459.67).slice(0,5) +"°F")
         wind.textContent = (data.current.wind_speed + " MPH")
         humidity.textContent = (data.current.humidity + "%")
-        uvi.textContent = (data.current.uvi)
-        console.log(data)
-        
+        uvi.textContent = (data.current.uvi + " UVI")
     })
 })
+
