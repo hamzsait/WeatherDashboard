47ed5740fa70afd55a35e3d29768ae748049aff6
hamzsait <hamzasait@utexas.edu>
2021-04-04 10:57:36 -0500

    finished the search for weather API text display

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,11 @@
+var getInput = document.querySelector("#input")
+var submit = document.querySelector("#submit")
+var title = document.querySelector("#cityTitle")
+var temp = document.querySelector("#temp")
+var wind = document.querySelector("#wind")
+var humidity = document.querySelector("#humidity")
+var uvi = document.querySelector("#uvi")
+
 async function getWeatherAPI(city){
 
     cityLonLat = await fetch("https://api.openweathermap.org/data/2.5/weather?q="+city+"&appid=fb58f472da0b2a8c35d45500aeb1b7c1")
@@ -18,19 +26,16 @@ async function getWeatherAPI(city){
     return outer
 }
 
-
-getWeatherAPI("Austin").then(function(data){
-    console.log(data)
-})
-
-var submit = document.querySelector("#submit")
-
-submit.textContent = "SEARCH"
-
-submit.addEventListener(event, function(){
+submit.addEventListener("click",function(event){
     event.preventDefault()
 
-    console.log("hello")
-
-    
+    getWeatherAPI(getInput.value).then(function(data){
+        title.textContent = getInput.value.toUpperCase()
+        temp.textContent = (String(data.current.temp*(9/5)-459.67).slice(0,5) +"°F")
+        wind.textContent = (data.current.wind_speed + " MPH")
+        humidity.textContent = (data.current.humidity + "%")
+        uvi.textContent = (data.current.uvi)
+        console.log(data)
+        
+    })
 })
